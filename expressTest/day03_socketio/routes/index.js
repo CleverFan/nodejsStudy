fc9356b03311b74e65c66b07f810ac310689b34a
17706446847 <377109151@qq.com>
2016-08-18 11:15:47 +0800

    add  chat

--- expressTest/day03_socketio/routes/index.js
+++ expressTest/day03_socketio/routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var socket_io = require('socket.io');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('response');
 });
+
+router.prepareSocketIO = function (server) {
+  var io = socket_io.listen(server);
+  io.sockets.on('connection',function (socket) {
+    socket.on('join',function (user) {
+      socket.user = user;
+      socket.emit('state','server',true);
+      socket.broadcast.emit('state','server',user+"online");
+    });
+
+    socket.on('sendMsg',function (msg) {
+      socket.emit('chat',socket.user,msg);
+      socket.broadcast.emit('chat',socket.user,msg);
+    });
+  });
+}
 
 module.exports = router;
